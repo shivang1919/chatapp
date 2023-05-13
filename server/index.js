4216e8bc0a0d8c4017c09cc522213f54a4236dfc
shivang <shivangnov@gmail.com>
2023-05-13 20:33:50 +0530

    First commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const http = require("http")
+const express = require("express")
+const cors = require("cors")
+const socketIO = require("socket.io")
+const dotenv = require("dotenv").config()
+const app = express()
+const port = process.env.PORT
+app.use(cors())
+app.get("/",(req,res)=>{
+    res.send("WELL IT'S WORKING")
+})
+const server = http.createServer(app)
+const io = socketIO(server);
+io.on("cnnection",()=>{
+    console.log("New Connection")
+})
+server.listen(port,()=>{
+    console.log(`Server is working on http://localhost:${port}`)
+})
